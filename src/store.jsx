1fcd97d4f5c6e235d42a3a0320f6e7d289e1a1fc
rafiqwe <rmlmanik502@gmail.com>
2025-05-28 18:27:12 +0600

    Create add Task Function in RTK

--- src/store.jsx
+++ src/store.jsx
@@ -8,15 +8,28 @@ const tasksReducer = createSlice({
   name: "task",
   initialState,
   reducers: {
-    addTask(state, action) {},
-    deleteTask(state, action) {},
+    addTask(state, action) {
+      state.task.push(action.payload);
+    },
+    deleteTask(state, action) {
+      state.task = state.task.filler(
+        (curElem, index) => index !== action.payload
+      );
+    },
   },
 });
+console.log(tasksReducer);
 
 const { addTask, deleteTask } = tasksReducer.actions;
 
 export const store = configureStore({
   reducer: {
-    tasksReducer,
+    tasksReducer: tasksReducer.reducer,
   },
 });
+// add some data 
+console.log(store.dispatch(addTask("I am a developer")));
+console.log(store.dispatch(addTask("I Know react ")));
+
+// Log all data
+console.log(store.getState());
